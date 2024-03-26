5af636efc47a76b9d4fba905667b1565ce101b72
victshev <victshev259@gmail.com>
2024-03-26 13:13:32 +0400

    Changes 1

--- playwright/NegativeTest.js
+++ playwright/NegativeTest.js
@@ -1,23 +1,20 @@
-const {
-    chromium
-  } = require(`playwright`);
-  const {
-    test,
-    expect
-  } = require(`@playwright/test`);
-  
-  (async () => {
-    const browser = await chromium.launch({
-        headless: false
-    });
-    const page = await browser.newPage();
-    await page.goto(`https://netology.ru/?modal=sign_in`);
-    await page.getByPlaceholder('Email').click();
-    await page.getByPlaceholder('Email').fill(`bhyjejd@mail.ru`);
-    await page.getByPlaceholder('Пароль').click();
-    await page.getByPlaceholder('Пароль').fill('123456789');
-    await page.getByTestId('login-submit-btn').click();
-    await expect(page.getByText(`Вы ввели неправильно логин или пароль`)).toBeVisible();
-  
-    await browser.close();
-  })();
+const { chromium } = require('playwright');
+const { email, password } = require('./user');
+
+(async () => {
+  const browser = await chromium.launch({
+    headless: false,
+    slowMo: 5000,
+    devtools: true
+  });
+
+  const page = await browser.newPage();
+  await page.goto('https://netology.ru/?modal=sign_in');
+  await page.fill('input[name="email"]', 'testtest@gmail.com');
+  await page.fill('input[name="password"]', '123456789');
+  await page.click('button[type="submit"]');
+  await page.pause();
+
+  //assertion
+  await browser.close();
+})();
--- playwright/PositiveTest.js
+++ playwright/PositiveTest.js
@@ -1,33 +1,21 @@
-const {
-    chromium
-  } = require(`playwright`);
-  const {
-    test,
-    expect
-  } = require("@playwright/test");
-  
-  const user = require("./user");
-  const email = user.userData.emailValue;
-  const password = user.userData.passwordValue;
-  
-  (async () => {
-    const browser = await chromium.launch({
-        headless: false
-    });
-    const context = await browser.newContext();
-    const page = await context.newPage();
-    await page.goto('https://netology.ru/?modal=sign_in');
-    await page.getByPlaceholder('Email').click();
-    await page.getByPlaceholder('Email').fill(email);
-    await page.getByPlaceholder('Пароль').click();
-    await page.getByPlaceholder('Пароль').fill(password);
-    await page.getByTestId('login-submit-btn').click();
-    await expect(page).toHaveURL(`https://netology.ru/profile`);
-    await page.waitForTimeout(5000);
-    await expect(page.getByRole('heading', {
-        name: 'Моё обучение'
-    })).toHaveText('Моё обучение');
-  
-    await context.close();
-    await browser.close();
-  })();
+const { chromium } = require('playwright');
+const { email, password } = require('./user');
+
+(async () => {
+  const browser = await chromium.launch({
+    headless: false,
+    slowMo: 5000,
+    devtools: true
+  });
+
+  const page = await browser.newPage();
+  await page.goto('https://netology.ru/?modal=sign_in');
+  await page.fill('input[name="email"]', email);
+  await page.fill('input[name="password"]', password);
+  await page.click('button[type="submit"]');
+  await page.pause();
+
+  //assertion
+  await browser.close();
+})();
+
